2161703c68e5162ea055f94b6e345310bb45bec8
Junior Alves <jjalves96@gmail.com>
2021-02-07 08:57:36 -0300

    feat: create template for Dashboard

---
+++ src/templates/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  margin-left: 24rem;
+  height: 100vh;
+  width: calc(100vw - 24rem);
+  padding: 4rem;
+`;
